5f2c32f6a577ec4c7682ea34d8f8001123dc13c6
chiragpoddar12 <chiragpoddar12@gmail.com>
2017-08-05 18:45:12 +0530

    Ratings Implemented

--- js/cardFlip.js
+++ js/cardFlip.js
@@ -3,6 +3,8 @@
   var hitCount = 0;
   var chances = 0;
   var selectedCards = [];
+  document.getElementById('score').innerHTML = "<h1>"+chances+"</h1>"
+  document.getElementById('rating').innerHTML = '<span class="glyphicon glyphicon-star" aria-hidden="true"></span><span class="glyphicon glyphicon-star" aria-hidden="true"></span><span class="glyphicon glyphicon-star" aria-hidden="true"></span>';
   var cards = document.querySelectorAll(".card.effect-click");
   for ( var i  = 0, len = cards.length; i < len; i++ ) {
     var card = cards[i];
@@ -22,6 +24,16 @@
       console.log(selectedCards);
       if(cardNumber == 2){
         chances++;
+        if(chances<10){
+          //3 stars
+          document.getElementById('rating').innerHTML = '<span class="glyphicon glyphicon-star" aria-hidden="true"></span><span class="glyphicon glyphicon-star" aria-hidden="true"></span><span class="glyphicon glyphicon-star" aria-hidden="true"></span>';
+        }else if(chances<14){
+          //2 stars
+          document.getElementById('rating').innerHTML = '<span class="glyphicon glyphicon-star" aria-hidden="true"></span><span class="glyphicon glyphicon-star" aria-hidden="true"></span>';
+        }else{
+          //1 star
+          document.getElementById('rating').innerHTML = '<span class="glyphicon glyphicon-star" aria-hidden="true"></span>';
+        }
         document.getElementById('score').innerHTML = "<h1>"+chances+"</h1>"
         if(selectedCards[1].getElementsByTagName('img')[0].src != selectedCards[0].getElementsByTagName('img')[0].src){
           setTimeout((reverseCards)(selectedCards),500);
